1b8c24d9d3e159fce3e0ef2a6528d5d8291c0381
박창준 <changjun.park@a-ha.io>
2025-02-17 11:29:16 +0900

    update counter context

---
+++ app/counter.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { PropsWithChildren } from "react";
+
+type Counter = {
+  count: number;
+  increment: () => void;
+};
+
+const CounterContext = React.createContext<Counter | null>(null);
+
+export function useCounter() {
+  const counter = React.useContext(CounterContext);
+
+  if (!counter) {
+    throw new Error("useCounter must be used within a CounterProvider");
+  }
+
+  return counter;
+}
+
+export function Counter(props: PropsWithChildren) {
+  const [count, setCount] = React.useState(0);
+  const id = React.useId();
+
+  const increment = React.useCallback(() => {
+    setCount((count) => count + 1);
+  }, []);
+
+  const memoizedValue = React.useMemo(
+    () => ({ count, increment }),
+    [count, increment]
+  );
+
+  return (
+    <CounterContext.Provider value={memoizedValue}>
+      {props.children}
+    </CounterContext.Provider>
+  );
+}
+
+export function CounterDisplay() {
+  const { count } = useCounter();
+
+  return <div>Count: {count}</div>;
+}
+
+export function CounterButton() {
+  const { increment } = useCounter();
+
+  return <button onClick={increment}>Increment</button>;
+}
---
+++ app/plain-component.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+export function PlainComponent() {
+  console.log("Plain component has been rendered");
+
+  return <div>I'm not using any context.</div>;
+}
